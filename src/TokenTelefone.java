8fdeeae42a6883cae8c8415547d34a224f3c98fb
joao <joao182cx@gmail.com>
2019-02-19 09:59:44 -0300

    Criação da leitura do número de telefone.

---
+++ src/TokenTelefone.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TokenTelefone {
+
+	public static void main(String[] args) {
+		
+		String numeroTelefone;
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Insira um numero no formato (555)555-5555:");
+		numeroTelefone = input.nextLine();
+
+	}
+
+}
