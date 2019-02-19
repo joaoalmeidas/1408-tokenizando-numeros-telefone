c80f4f6d1f2d167ed34f1639a1d390112cac5dc4
joao <joao182cx@gmail.com>
2019-02-19 10:18:50 -0300

    Criação do código que criará os tokens do numero inserido pelo usuário e que depois irá exibir na tela os tokens separados e tambem concatenados.

--- src/TokenTelefone.java
+++ src/TokenTelefone.java
@@ -5,13 +5,28 @@ public class TokenTelefone {
 	public static void main(String[] args) {
 		
 		String numeroTelefone;
-		
+		String[] token;
+		String numeroConcatenado = "";
 		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Insira um numero no formato (555)555-5555:");
 		numeroTelefone = input.nextLine();
-
+		numeroTelefone = numeroTelefone.substring(1);
+		
+		token = numeroTelefone.split("\\D");
+		
+		System.out.printf("Código de área: %s%n", token[0]);
+		System.out.printf("Primeira parte do telefone: %s%n", token[1]);
+		System.out.printf("Segunda parte do telefone: %s%n", token[2]);
+		
+		for(String numero : token) {
+			
+			numeroConcatenado += numero;
+			
+		}
+		
+		System.out.printf("Númerdo concatenado: %s", numeroConcatenado);
 	}
 
 }
